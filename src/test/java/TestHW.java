a429f4be3690d9104ce652beba817358bd9e93bc
unknown <chitava@mail.ru>
2023-10-22 14:45:00 +0300

    Сделал 1 и 2 задачу

---
+++ src/test/java/TestHW.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestHW {
+    MainHW newExempl = new MainHW();
+    @Test
+    void testEvent(){
+        assertThat(newExempl.EventOrNot(2)).isTrue();
+    }
+
+    @Test
+    void testNotEvent(){
+        assertThat(newExempl.EventOrNot(3)).isFalse();
+    }
+
+
+    @Test
+    void testNotInterval(){
+        assertThat(newExempl.isInInterval(10)).isFalse();
+    }
+
+    @Test
+    void testInInterval(){
+        assertThat(newExempl.isInInterval(30)).isTrue();
+    }
+
+}
